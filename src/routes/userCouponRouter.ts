3a18d3610266ccf1e0314db496cde29bb8ed97a0
Pedro Brennon <pbrennon98@gmail.com>
2021-08-28 10:43:40 -0300

    Fix url userCoupon/user

--- src/routes/userCouponRouter.ts
+++ src/routes/userCouponRouter.ts
@@ -6,7 +6,7 @@ const routes = Router();
 
 routes.get('/', userCouponController.get);
 routes.get('/:id', userCouponController.getById);
-routes.get('user/:userId', userCouponController.getByUserId);
+routes.get('/user/:userId', userCouponController.getByUserId);
 routes.post('/', userCouponController.insert);
 routes.put('/:id', auth.verifyJWT, userCouponController.udpate);
 routes.delete('/:id', auth.verifyJWT, userCouponController.delete);
